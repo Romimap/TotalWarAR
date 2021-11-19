58a1285ea87c3b3026c86e0f0aea1bfb819d08af
Romain Fournier <romain.fournier.095@gmail.com>
2021-11-19 20:55:02 +0100

    Setup anchor and prefab placement, to try

--- Total War AR/Assets/Scripts/PlacingController.cs
+++ Total War AR/Assets/Scripts/PlacingController.cs
@@ -6,6 +6,7 @@ using UnityEngine.XR.ARFoundation;
 public class PlacingController : MonoBehaviour {
     public ARRaycastManager raycastManager;
     public ARAnchorManager anchorManager;
+    public ARPlaneManager planeManager;
 
     ARAnchor anchor = null;
 
@@ -25,10 +26,39 @@ public class PlacingController : MonoBehaviour {
         
     }
 
-    void Place (Vector2 screenPosition) {
+    void Place (Vector2 screenPosition, GameObject prefab) {
+        if (anchor == null) {
+            Debug.LogError("Set anchor first!");
+            return;
+        }
+
         List<ARRaycastHit> results = new List<ARRaycastHit>();
         if (raycastManager.Raycast(screenPosition, results)) {
+            foreach (ARRaycastHit result in results) {
+                if (result.trackableId == anchor.trackableId) {
+                    GameObject instance = Instantiate(prefab);
+                    instance.transform.parent = anchor.transform;
+                    instance.transform.position = result.pose.position;
+                    break;
+                }
+            }
+        }
+    }
 
+    void SetAnchor (Vector2 screenPosition) {
+        if (anchor != null) {
+            Debug.LogError("Anchor already set !");
+            return;
+        }
+
+        List<ARRaycastHit> results = new List<ARRaycastHit>();
+        if (raycastManager.Raycast(screenPosition, results)) {
+            ARTrackable trackable = results[0].trackable;
+            Pose pose = results[0].pose;
+            if (trackable is ARPlane) {
+                anchor = anchorManager.AttachAnchor((ARPlane)trackable, pose);
+                planeManager.detectionMode = UnityEngine.XR.ARSubsystems.PlaneDetectionMode.None; //Stop plane detection as soon as we have our anchor
+            }
         }
     }
 }
