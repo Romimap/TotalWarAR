b332c7bd8aa213268f3b0e0a0603043ce1aa845a
Romimap <romain.fournier.095@gmail.com>
2021-11-19 13:08:07 +0100

    :)

---
+++ Total War AR/Assets/Scripts/PlacingController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PlacingController : MonoBehaviour {
+    public ARRaycastManager raycastManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Place () {
+        
+    }
+}
